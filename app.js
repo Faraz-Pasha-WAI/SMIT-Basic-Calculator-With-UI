84e95e403c9a4f0e3c7823c0d85bb742650dcb38
faraz pasha <farazpasha1990@gmail.com>
2025-08-31 07:24:14 +0500

    fourth

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 let display = document.getElementById("display");
 
+let forClearScreen = false;
+
 function appendvalue(val) {
+  let lastValue = display.value.slice(-1);
+  let opt = ["+", "-", "*", "/", "%", "."];
+
+  let isOpt = false;
+  let isLastOpt = false;
+
+  for (let i = 0; i < opt.length; i++) {
+    if (val === opt[i]) {
+      isOpt = true;
+    }
+    if (lastValue === opt[i]) {
+      isLastOpt = true;
+      break;
+    }
+  }
+
+  if (isOpt && isLastOpt) {
+    return;
+  }
+
+ 
+  if (justCalculated === true && isOpt === false) {
+    display.value = "";
+  }
+  justCalculated = false;
+
+
   display.value += val;
 }
+
+
 
 function clearvalue() {
   display.value = "";
